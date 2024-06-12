78543c56420a57e6b5770dd79616a125e6847201
ozangenc <ozantrb61ziganoylu@gmail.com>
2024-06-12 20:07:54 +0200

    fin

--- routes.py
+++ routes.py
@@ -12,23 +12,19 @@ def accueil():
                            liste_cat=liste_categories
                            )
 
+
 @app.route('/tous_produits')
 def produits():
     liste_produits = models.vue_produits_categories.query.all()
     return render_template('tous_produits.html', title='Easy Clothes', liste_prod=liste_produits)
 
 
-@app.route('/produits_categorie')
-def produits_categorie():
-    id_cat = request.args.get('id_cat')
-    liste_produits = models.vue_produits_categories.query.filter_by(id_cat=id_cat)
-    return render_template('produits_categorie.html', title='Easy Clothes', produits=liste_produits,
-                           typeprod=type(liste_produits))
-
 @app.route('/creation')
 def creations():
     liste_creations = models.vue_creation.query.all()
     return render_template('creation.html', title='Easy Clothes', liste_crea=liste_creations)
+
+
 @app.route('/nouveaute')
 def nouveautes():
     liste_nouveaute = models.vue_nouveaute.query.all()
